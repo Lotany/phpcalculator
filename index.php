6ecec5604ac7436d65d4fe25c16ef276dd674d9d
Able <ablekilinda@gmail.com>
2022-10-19 21:13:39 +0300

    loan

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
     </head>
 
     <body>
+        <div class="header">
         <nav class="header-1">
             <ul>
                 <li>Calculator</li>
             </ul>
         </nav>
+
+        </div>
+
 
         <h1 class="head">Perfom your calculations here</h1>
         
@@ -21,27 +25,25 @@
                 <input class="one" type="text" name="num1" placeholder="Enter the first number">
                 <br>
                 <input class="two" type="text" name="num2" placeholder="Enter the second number"><br>
-                <button class="right" type="submit" name="submit">Calculate</button>
-            </form>
-        </section>
-
-        <section class="selector1">
-            <div class="selector2">
-            <select name="select" id="select">
+               
+                <div class="select">
+                <select name="select" id="select">
                 <option value="none">None</option>
                 <option value="add">Add</option>
                 <option value="sub">Subtract</option>
                 <option value="mult">Multiply</option>
                 <option value="div">Divide</option>
                 <option value="rem">Remainder</option>
-            </select>
-            </div>
+                </select>
+                </div>
 
-
+               
+                <button class="right" type="submit" name="submit">Calculate</button>
+            </form>
         </section>
 
 
-    <div class="foooter">
+    <div class="footer">
         <footer>
             <a href="#">Designed by Able Kilinda</a>
         </footer>
