6179267d7b11204480666f17498dc90eabecd664
Able <ablekilinda@gmail.com>
2022-10-19 18:16:38 +0300

    adding html

--- index.php
+++ index.php
@@ -11,8 +11,24 @@
             <ul>
                 <li>Calculator</li>
             </ul>
-
         </nav>
+
+        <h1>Perfom your calculations here</h1>
         
-    </body>
+        <section class="section1">
+            <form action="GET" method="GET">
+                <input type="text" name="num1" placeholder="Enter the first number"><br>
+                <input type="text" name="num2" placeholder="Enter the second number"><br>
+                <button type="submit" name="submit">Calculate</button>
+            </form>
+        </section>
+
+        <section class="selector1">
+            <select name="select" id="select">
+                <li>Add</li>
+                <li>None</li>
+            </select>
+
+        </section>
+        </body>
 </html>
