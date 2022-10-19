672b571c1603de1e5e1fab804c1148a60c0e0720
Able <ablekilinda@gmail.com>
2022-10-19 18:10:11 +0300

    first add to server

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta viewport= "">
+        <title>php calculator</title>
+    </head>
+
+    <body>
+        <nav class="header-1">
+            <ul>
+                <li>Calculator</li>
+            </ul>
+
+        </nav>
+        
+    </body>
+</html>
