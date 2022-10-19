84c9fb968fcebf3b9b7e29ba675b26280ca1b22e
Able <ablekilinda@gmail.com>
2022-10-19 21:53:11 +0300

    lotan

--- index.php
+++ index.php
@@ -4,13 +4,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>php calculator</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <nav>
         <div class="navbar">
-            <a href="#">Home</a>
-            <a href="#">News</a>
-            <a href="#">About</a>
+            <h2 class="logo"><a href="#">My Program</a></h2>
+           <ul class="navbarul">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
         </div>
+        </nav>
+
+        <div class="section1">
+            <form action="">
+                <option value="non">None</option>
+                <option value="non">None</option>
+                <option value="non">None</option>
+                <option value="non">None</option>
+                <option value="non">None</option>
+                <option value="non">None</option>
+            </form>
+
+        </div>
+
         
     </body>
 </html>
